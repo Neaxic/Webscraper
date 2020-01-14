a8b231516347716879fe6458f10742991a58adea
Neaxic <andreas79001@gmail.com>
2020-01-14 14:01:35 +0100

    Parser as function

--- potusParse.js
+++ potusParse.js
@@ -1,18 +1,36 @@
+//https://www.freecodecamp.org/news/the-ultimate-guide-to-web-scraping-with-node-js-daa2027dcd3/
 
 const rp = require('request-promise');
 const $ = require('cheerio');
 const url = 'https://en.wikipedia.org/wiki/George_Washington';
 
-rp(url)
-    .then(function(html) {
-        //console.log(html);
-        const element = $('#firstHeading',html)
-        console.log(element.text());
 
-        const bday = ($('.bday',html).text());
-        console.log(bday);
-    })
-    .catch(function(err) {
+// function PotusParse(url) {
+//     let retVal = {
+//         name = 'test',
+//         birthday = 'kdkdk',
+//     }
 
-    })
+//     return retVal
+// }
 
+
+function PotusParse(url) {
+    return rp(url)
+        .then(function (html) {
+            //console.log(html);
+            const name = ($('#firstHeading', html).text());
+            console.log(element.text());
+
+            const bday = ($('.bday', html).text());
+            console.log(bday);
+
+            svar = name, bday;
+            return svar;
+        })
+        .catch(function (err) {
+            console.log('Error: ', err)
+        })
+}
+
+module.exports(PotusParse);
--- potusScraper.js
+++ potusScraper.js
@@ -1,23 +1,35 @@
+//https://www.freecodecamp.org/news/the-ultimate-guide-to-web-scraping-with-node-js-daa2027dcd3/
+
 const rp = require('request-promise');
 const url = 'https://en.wikipedia.org/wiki/List_of_Presidents_of_the_United_States';
 const $ = require('cheerio');
+const PotusParse = require('./potusParse');
 
 const content = [];
 
 
 rp(url)
-    .then(function(html){
+    .then(function (html) {
         //console.log(html);
         console.log($('big > a', html).length);
-        for(let i = 0; i < 45; i++){
-            content.push($('big > a', html) [i].attribs.href);
+        for (let i = 0; i < 45; i++) {
+            content.push($('big > a', html)[i].attribs.href);
         }
-
-        console.log(content);
+        return Promise.all(
+            wikiUrls.map(function (url) {
+                return PotusParse('https://en.wikipedia.org' + url);
+            })
+        );
     })
-    .catch(function(err){
+    .then(function(presidents){
+    console.log(presidents);
+    })
+    .catch(function (err) {
+        console.log('Error: ', err)
     });
 
+
+const svar = PotusParse()
 
 
 //console.log('hi')
